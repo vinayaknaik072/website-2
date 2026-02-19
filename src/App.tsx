4fdb588093654bd566b469263f4f825b18528079
vinayaknaik072 <vinayaknaik072@gmail.com>
2026-02-19 14:28:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import hello from'./Component1/hello'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='app'></div>
+      <hello/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component1/hello.tsx
@@ -0,0 +1,7 @@
+import"./hello.css"
+function hello(){
+    return(
+        <>
+        </>
+    )
+}
