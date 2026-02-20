43846ad9050b9db1b961e4ee62e6fab129007361
vinayaknaik072 <vinayaknaik072@gmail.com>
2026-02-20 14:05:30 +0530

    first commit

--- src/Component/hello.tsx
+++ src/Component/hello.tsx
@@ -1,6 +1,6 @@
 import"./hello.css"
 import img from '../assets/arrow.png'
-import img1 from '../assets/profile1.png'
+import img1 from '../assets/profile2.png'
 import img2 from '../assets/greater then.png'
 import img3 from '../assets/door icon.png'
 function hello(){
@@ -9,7 +9,7 @@ function hello(){
         <div className="main">
         <div className="top">
             <button className="left">
-                <img src={img} alt="" />
+                <img src={img} alt=""className="arrow1" />
             </button>
             <div className="right"><h3>Profile</h3></div>
         </div>
@@ -17,19 +17,19 @@ function hello(){
             <div className="card">
                 <div className="top1">
                     <div className="top2">
-                        <div className="circle"><h1>AD</h1>
-                            <div className="circle1"></div>
+                        <div className="circle">AD
                         </div>
+                            <div className="circle1"></div>
                     </div>
                     <div className="middle2"><h3>Admin</h3></div>
-                    <div className="bottom2"><p>admin@text.com</p></div>
+                    <div className="bottom2"><p>admin@test.com</p></div>
                 </div>
                 <div className="middle1">
                     <div className="left3">
                         <div className="left4">
                             <img src={img1} alt=""className="profile" />
                         </div>
-                        <div className="right4"><h3>Edit Profile</h3></div>
+                        <div className="right4"><h4>Edit Profile</h4></div>
                     </div>
                     <div className="right3">
                         <div className="arrow">
@@ -41,7 +41,7 @@ function hello(){
                     <div className="left5">
                         <img src={img3} alt="" className="door"/>
                     </div>
-                    <div className="right5"><h3>Logout</h3></div>
+                    <div className="right5"><h4>Logout</h4></div>
                 </div>
             </div>
         </div>
